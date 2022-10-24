5f90d9edef0b4ed38fd6786705802bc9f108226a
Elisee-dev <esabidani@gmail.com>
2022-10-24 01:01:42 +0000

    utilisation des assets OK

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -5,9 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon site</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('partials.navbar')
     @yield('content')
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
